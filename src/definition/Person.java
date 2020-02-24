a214db3eb7514008a40c778116bd6d931d214ceb
manish2407 <manish.sharma_cs18@gla.ac.in>
2020-02-24 21:49:43 +0530

    we create  a person in definition package

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,7 +1,7 @@
 package definition;
 
 public class Person {
-    //Declaring all the Variables , that will get used
+
     private String contactFirstName;
     private String contactLastName;
     private String[] contactNumber;
@@ -13,7 +13,7 @@ public class Person {
         this.contactNumber = contactNumber;
         this.emailOfContact = emailOfContact;
     }
-    //Creating Getters.
+
     public String getContactFirstName() {
         return contactFirstName;
     }
@@ -30,7 +30,7 @@ public class Person {
         return emailOfContact;
     }
 
-    //Created Setters
+
     public void setContactFirstName(String contactFirstName) {
         this.contactFirstName = contactFirstName;
     }
@@ -46,31 +46,11 @@ public class Person {
     public void setEmailOfContact(String emailOfContact) {
         this.emailOfContact = emailOfContact;
     }
-    /**
-     * Returns a string representation of the object. In general, the
-     * {@code toString} method returns a string that
-     * "textually represents" this object. The result should
-     * be a concise but informative representation that is easy for a
-     * person to read.
-     * It is recommended that all subclasses override this method.
-     * <p>
-     * The {@code toString} method for class {@code Object}
-     * returns a string consisting of the name of the class of which the
-     * object is an instance, the at-sign character `{@code @}', and
-     * the unsigned hexadecimal representation of the hash code of the
-     * object. In other words, this method returns a string equal to the
-     * value of:
-     * <blockquote>
-     * <pre>
-     * getClass().getName() + '@' + Integer.toHexString(hashCode())
-     * </pre></blockquote>
-     *
-     * @return  a string representation of the object.
-     */
+
 
 
     public String toString(){
-        //System.out.println("---Here are all your contacts---");
+
         final StringBuffer strafe = new StringBuffer("-------- * -------- * -------- * --------\n");
         strafe.append("First Name: ").append(contactFirstName).append("\n");
         strafe.append("Last Name: ").append(contactLastName).append("\n");
